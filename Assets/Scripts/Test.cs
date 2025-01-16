3da87d4674d65446cca49b17283bb271eda4fcb2
cumic06 <changhwan060224@gmail.com>
2025-01-17 00:16:01 +0900

    1차 테스트
    NativeArrayTest

--- Assets/Scripts/Test.cs
+++ Assets/Scripts/Test.cs
@@ -1,6 +1,22 @@
+using Unity.Collections;
 using UnityEngine;
 
 public class Test : MonoBehaviour
 {
-    
+    private NativeArray<int> navtiveArray;
+
+    private void Start()
+    {
+        navtiveArray = new NativeArray<int>(10, Allocator.Persistent);
+        TestCode();
+    }
+
+    private void TestCode()
+    {
+        for (int i = 0; i < navtiveArray.Length; i++)
+        {
+            navtiveArray[i] = i;
+        }
+        navtiveArray.Dispose();
+    }
 }
