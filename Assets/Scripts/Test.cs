896941f221f73457c9ec925a628e65d6eb25b728
cumic06 <92432064+cumic06@users.noreply.github.com>
2025-01-18 14:52:52 +0900

    2차 테스트
    배열 사이즈 값이 늘어날수록 기하급수적으로 효과가 늘어남

--- Assets/Scripts/Test.cs
+++ Assets/Scripts/Test.cs
@@ -1,22 +1,50 @@
+using System.Diagnostics;
+using Unity.Burst;
 using Unity.Collections;
+using Unity.Jobs;
 using UnityEngine;
+using Debug = UnityEngine.Debug;
 
 public class Test : MonoBehaviour
 {
-    private NativeArray<int> navtiveArray;
-
     private void Start()
     {
-        navtiveArray = new NativeArray<int>(10, Allocator.Persistent);
-        TestCode();
+        int size = 100000000;
+        Stopwatch stopwatch = new Stopwatch();
+        stopwatch.Start();
+        int[] intArray = new int[size];
+        for (int i = 0; i < intArray.Length; i++)
+        {
+            intArray[i] = (int)Mathf.Pow(i, i);
+        }
+        stopwatch.Stop();
+        Debug.Log($"milliseconds : {stopwatch.ElapsedMilliseconds}");
+
+        stopwatch.Reset();
+        stopwatch.Start();
+        NativeArray<int> nativeArray = new NativeArray<int>(size, Allocator.TempJob);
+        var job = new BurstJob() { nativeArray = nativeArray };
+        job.Schedule(nativeArray.Length, 64).Complete();
+        stopwatch.Stop();
+        Debug.Log($"milliseconds : {stopwatch.ElapsedMilliseconds}");
+        nativeArray.Dispose();
     }
 
-    private void TestCode()
+    [BurstCompile]
+    struct BurstJob : IJobParallelFor
     {
-        for (int i = 0; i < navtiveArray.Length; i++)
+        public NativeArray<int> nativeArray;
+
+        public void Execute(int index)
         {
-            navtiveArray[i] = i;
+            nativeArray[index] = (int)Mathf.Pow(index, index);
+            ;
+            // for (int i = 0;
+            //      i < nativeArray.Length;
+            //      i++)
+            // {
+            //     nativeArray[i] = (int)Mathf.Pow(i, i);
+            // }
         }
-        navtiveArray.Dispose();
     }
 }
